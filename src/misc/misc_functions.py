d29e9b2114d25e07e19709a3402ca04d4014710b
Pankaj Gupta <pankaj@Pankajs-PC.local>
2022-06-20 00:21:35 +0530

    altaar gameplay fixes

--- src/misc/misc_functions.py
+++ src/misc/misc_functions.py
@@ -13,13 +13,78 @@ BASE_RATE = [20, 10, 5, 2] #ordered by FUD, FOMO, ALPHA, KEK
 plot_size_array = [1, 2, 4, 8, 16, 32, 64]
 SPILLOVER_COLLECTION_RATE = 0.3 # this will depend on radius and size of parcel
 
-URL = "https://api.coingecko.com/api/v3/simple/price?ids=aavegotchi-fud%2Caavegotchi-fomo%2Caavegotchi-alpha%2Caavegotchi-kek%2Caavegotchi&vs_currencies=usd"
-result = requests.get(url = URL).json()
 
-fud_factor = 1.0
-fomo_factor = result['aavegotchi-fomo']['usd']/result['aavegotchi-fud']['usd']
-alpha_factor = result['aavegotchi-alpha']['usd']/result['aavegotchi-fud']['usd']
-kek_factor = result['aavegotchi-kek']['usd']/result['aavegotchi-fud']['usd']
+
+
+try:
+    URL = "https://api.coingecko.com/api/v3/simple/price?ids=aavegotchi-fud%2Caavegotchi-fomo%2Caavegotchi-alpha%2Caavegotchi-kek%2Caavegotchi&vs_currencies=usd"
+    result = requests.get(url = URL).json()
+
+    fud_factor = 1.0
+    fomo_factor = result['aavegotchi-fomo']['usd']/result['aavegotchi-fud']['usd']
+    alpha_factor = result['aavegotchi-alpha']['usd']/result['aavegotchi-fud']['usd']
+    kek_factor = result['aavegotchi-kek']['usd']/result['aavegotchi-fud']['usd']
+except:
+    print("unable to get token factors from the API")
+    fud_factor = 1.0
+    fomo_factor = 2.0
+    alpha_factor = 10.0
+    kek_factor = 20.0
+
+
+class Yield:
+    fud = 0
+    fomo = 0
+    alpha = 0
+    kek = 0
+    def __init__(self, fud = 0, fomo = 0, alpha = 0, kek = 0):
+        self.fud = fud
+        self.fomo = fomo
+        self.alpha = alpha
+        self.kek = kek
+        
+    
+    def add_yield(self, y1):
+        self.fud += y1.fud
+        self.fomo += y1.fomo
+        self.alpha += y1.alpha
+        self.kek += y1.kek
+        
+    def substract_yield(self, y1):
+        self.fud -= y1.fud
+        self.fomo -= y1.fomo
+        self.alpha -= y1.alpha
+        self.kek -= y1.kek
+        
+    def add_yield_by_base_rate(self, base_rate, factor):
+        # base rate is a 4 dimensional array
+        self.fud += base_rate[0] * factor
+        self.fomo += base_rate[1] * factor
+        self.alpha += base_rate[2] * factor
+        self.kek += base_rate[3] * factor
+        
+    def get_total_yield_in_fud(self):
+        try:
+            URL = "https://api.coingecko.com/api/v3/simple/price?ids=aavegotchi-fud%2Caavegotchi-fomo%2Caavegotchi-alpha%2Caavegotchi-kek%2Caavegotchi&vs_currencies=usd"
+            result = requests.get(url = URL).json()
+
+            fud_factor = 1.0
+            fomo_factor = result['aavegotchi-fomo']['usd']/result['aavegotchi-fud']['usd']
+            alpha_factor = result['aavegotchi-alpha']['usd']/result['aavegotchi-fud']['usd']
+            kek_factor = result['aavegotchi-kek']['usd']/result['aavegotchi-fud']['usd']
+        except:
+            print("unable to get token factors from the API")
+            fud_factor = 1.0
+            fomo_factor = 2.0
+            alpha_factor = 10.0
+            kek_factor = 20.0
+            
+        return fud_factor*self.fud + fomo_factor*self.fomo + alpha_factor*self.alpha + kek_factor*self.kek
+    
+    def diplay(self):
+        print("FUD: {}\nFOMO: {}\nALPHA: {}\nKEK: {}".format(self.fud, self.fomo, self.alpha, self.kek))
+
+
 
 
 CHANNELING_RATE_IN_HOURS = {0: float('inf'),
@@ -27,12 +92,23 @@ CHANNELING_RATE_IN_HOURS = {0: float('inf'),
     5:6, 6:4, 7:3, 8:2, 9:1
 }
 
+INCLUDE_SPILLOVER_COMPUTATION = False
+
 
 altaar_cost_df = pd.read_csv("data/altaar_cost.csv")
 altaar_cost_df = altaar_cost_df.replace(',','', regex=True)
 
 altaar_built_time_dict = { altaar_cost_df['Level'][i]: altaar_cost_df['Buid Time (Days) '][i] for i in range(altaar_cost_df.shape[0]) }
-altaar_built_time_dict
+
+altaar_built_cost_dict_in_alchemica = {
+    altaar_cost_df['Level'][i]: Yield(fud = int(altaar_cost_df['FUD'][i]),
+                                      fomo = int(altaar_cost_df['FOMO'][i]),
+                                      alpha = int(altaar_cost_df['ALPHA'][i]),
+                                      kek = int(altaar_cost_df['KEK'][i]),
+                                     )
+    for i in range(altaar_cost_df.shape[0])
+}
+
 
 
 altaar_built_cost_dict = { altaar_cost_df['Level'][i]: 
@@ -41,7 +117,6 @@ altaar_built_cost_dict = { altaar_cost_df['Level'][i]:
                           alpha_factor*int(altaar_cost_df['ALPHA'][i])+
                           kek_factor*int(altaar_cost_df['KEK'][i]) 
                           for i in range(altaar_cost_df.shape[0]) }
-altaar_built_cost_dict
 
 
 yield_at_base_rate = fud_factor*BASE_RATE[0]+ \
@@ -92,10 +167,19 @@ This function computes yield which an altaar which generate in 1 day, given the 
 2. the level of altaar
 3. dimension of the parcel
 
-yield = frequency_of_channel x base_rate_yield x channeling_modifier x spillover_rate x collection_rate
+yield = yield_from_altaar + yield_from_spillover,
+
+where,
+
+actual_yield = frequency_of_channel x base_rate_yield x channeling_modifier
+yield_from_altaar = actual_yield x (1-spillover_rate)
+yield_from_spillover = actual_yield x spillover_rate x collection_rate
 """
 
 def get_yield_for_the_day(gotchi_kinship_array, altaar_level, parcel_dim):
+    
+    yield_obj = Yield()
+    
     x, y = parcel_dim
     
     spillover_rate = (50 - (altaar_level-1)*5)/100.0
@@ -105,12 +189,18 @@ def get_yield_for_the_day(gotchi_kinship_array, altaar_level, parcel_dim):
     channel_frequency = int(24/CHANNELING_RATE_IN_HOURS[altaar_level])
     
     total_yield = 0
-    for i in range(min(len(gotchi_kinship_array), channel_frequency)):
-        total_yield += int(channeling_modifier_array[i]*yield_at_base_rate*(1-spillover_rate) * spill_collection_rate(x, y))
+    for i in range(min(len(gotchi_kinship_array), int(channel_frequency))):
+        
+        #yield from the altaar
+        total_yield += int(channeling_modifier_array[i]*yield_at_base_rate*(1-spillover_rate))
+        
+        yield_obj.add_yield_by_base_rate(BASE_RATE, channeling_modifier_array[i]*(1-spillover_rate))
+        
+        if INCLUDE_SPILLOVER_COMPUTATION:
+            total_yield += int(channeling_modifier_array[i]*yield_at_base_rate*spillover_rate * spill_collection_rate(x, y))
+            yield_obj.add_yield_by_base_rate(BASE_RATE, channeling_modifier_array[i]*spillover_rate * spill_collection_rate(x, y))
     
-    return total_yield
-
-get_yield_for_the_day([500, 500], 1, [64,64])
+    return total_yield, yield_obj
 
 
 
@@ -124,17 +214,118 @@ def get_yield_for_the_upgrade_day(gotchi_kinship_array, altaar_level, parcel_dim
     # using the same function as of now, will work on this function later
     return get_yield_for_the_day(gotchi_kinship_array, altaar_level, parcel_dim)
 
-get_yield_for_the_upgrade_day([500, 500], 1, [64,64], 0.3)
 
 
 def future_yield_by_current_altaar(gotchi_kinship_array, altaar_level, parcel_dim, num_days):
-    return get_yield_for_the_day(gotchi_kinship_array, altaar_level, parcel_dim)*num_days
+    
+    gotchi_array = gotchi_kinship_array.copy()
+    
+    yield_till_now = 0
+    for i in range(num_days):
+        yield_till_now += get_yield_for_the_day(gotchi_array, altaar_level, parcel_dim)[0]
+        gotchi_array[:] = map(lambda x: x+170, gotchi_array) # gotchi kinships increase by 2 everyday
+        
+    return yield_till_now
+
+
+
+def get_breakeven_time(Parcels, num_days):
+    
+    daywise_profit_yield_array = [0]*num_days
+    
+    
+    
+    
+    
+    for parcel in Parcels:
+        total_yield_till_now = 0
+        profit_yield_till_now = 0
+        yield_obj = Yield()
+        profit_yield_obj = Yield()
+        
+        parcel_dim = parcel.dim
+        target_altaar_level = parcel.altaar_level
+        gotchi_kinship_array = parcel.gotchi_kinship_array
+                
+        current_altaar = 1
+        pending_built_time_for_next_altaar = math.inf if current_altaar >= target_altaar_level else altaar_built_time_dict[current_altaar+1]
+        
+        for i in range(num_days):
+            
+            if current_altaar > target_altaar_level:
+                break
+
+            if pending_built_time_for_next_altaar <=1:
+                
+                t1 = get_yield_for_the_upgrade_day(gotchi_kinship_array, current_altaar, parcel_dim, pending_built_time_for_next_altaar)
+                
+                yield_obj.add_yield(t1[1])
+                profit_yield_obj.add_yield(t1[1])
+                profit_yield_obj.substract_yield(altaar_built_cost_dict_in_alchemica[current_altaar+1])
+                
+                altaar_built_cost_dict_in_alchemica
+                
+                total_yield_till_now += t1[0]
+                profit_yield_till_now += t1[0] - altaar_built_cost_dict[current_altaar+1] 
+
+                if current_altaar == target_altaar_level-1:
+                    current_altaar = current_altaar + 1
+                    pending_built_time_for_next_altaar = math.inf
+                elif current_altaar == target_altaar_level:
+                    pending_built_time_for_next_altaar = math.inf
+                else:
+                    current_altaar = current_altaar + 1
+                    pending_built_time_for_next_altaar = altaar_built_time_dict[current_altaar+1]
+
+            else:
+                t1 = get_yield_for_the_day(gotchi_kinship_array, current_altaar, parcel_dim)
+
+                yield_obj.add_yield(t1[1])
+                profit_yield_obj.add_yield(t1[1])
+                
+                total_yield_till_now += t1[0]
+                profit_yield_till_now += t1[0]
+                
+                pending_built_time_for_next_altaar -= 1
+
+                            
+
+            daywise_profit_yield_array[i] += profit_yield_till_now
+            gotchi_kinship_array = [ele+2 for ele in gotchi_kinship_array]
+        
+            #print("day {}, current level {}, pending time {}, current total yield {}".format(i+1, current_altaar, pending_built_time_for_next_altaar, total_yield_till_now))
+    
+    
+
+#     print("----------------------------------------")
+#     print("total yield --> {}, profit yield --> {}".format(total_yield_till_now, profit_yield_till_now))
+#     print("\n")
+#     yield_obj.diplay()
+#     print(yield_obj.get_total_yield_in_fud())
+
+#     print("\n")
+#     profit_yield_obj.diplay()
+#     print(profit_yield_obj.get_total_yield_in_fud())
+    
+    
+    last_neg = 0
+    for i, ele in enumerate(daywise_profit_yield_array):
+        if ele<=0:
+            last_neg = i
+            
+    
+    #print(daywise_total_yield_array)
+
+    if last_neg == num_days-1:
+        return -1, yield_obj, profit_yield_obj
+    
+    return last_neg, yield_obj, profit_yield_obj
 
 
 
 
 """
-Given (a) number of days (b) what could be the maximum altaar built,
+Given (a) number of days (b) maximum allowed altaar built,
 this function computes the maximum yield possible, and till what level should the altaar be upgraded
 """
 def gameplay_v3(gotchi_kinship_array, num_days, current_altaar, parcel_dim, max_allowed_altaar):
@@ -144,13 +335,18 @@ def gameplay_v3(gotchi_kinship_array, num_days, current_altaar, parcel_dim, max_
     total_yield_till_now = 0
     
     max_expected_yield = 0
-    max_yield_level = 1
+    max_yield_level = 0
     
     for i in range(num_days):
-        
+                
         if pending_built_time_for_next_altaar <=1: #altaar can be made current day
-            total_yield_till_now += get_yield_for_the_upgrade_day(gotchi_kinship_array, current_altaar, parcel_dim, pending_built_time_for_next_altaar)
+            
+            total_yield_till_now += get_yield_for_the_upgrade_day(gotchi_kinship_array, current_altaar, parcel_dim, pending_built_time_for_next_altaar)[0]
+            
+            #print("kinship array {}, altaar level {} parcel dim {} pendinng time {} total yield {}".format(gotchi_kinship_array, current_altaar, parcel_dim, pending_built_time_for_next_altaar, get_yield_for_the_upgrade_day(gotchi_kinship_array, current_altaar, parcel_dim, pending_built_time_for_next_altaar)))
+
             total_yield_till_now -= altaar_built_cost_dict[current_altaar+1]  #reducing the total cost for building the altaar
+            #print("came here {} {}".format(altaar_built_cost_dict[current_altaar+1], total_yield_till_now))
             
             if current_altaar == max_allowed_altaar-1:
                 current_altaar = current_altaar + 1
@@ -162,30 +358,23 @@ def gameplay_v3(gotchi_kinship_array, num_days, current_altaar, parcel_dim, max_
                 pending_built_time_for_next_altaar = altaar_built_time_dict[current_altaar+1]
 
         else:
-            total_yield_till_now += get_yield_for_the_day(gotchi_kinship_array, current_altaar, parcel_dim)
+            total_yield_till_now += get_yield_for_the_day(gotchi_kinship_array, current_altaar, parcel_dim)[0]
             pending_built_time_for_next_altaar -= 1
             
-        #print("day {}, current level {}, pending time {}".format(i+1, current_altaar, pending_built_time_for_next_altaar))
+            
+        #print("day {}, current level {}, pending time {}, current total yield {}".format(i+1, current_altaar, pending_built_time_for_next_altaar, total_yield_till_now))
         
         future_yield = future_yield_by_current_altaar(gotchi_kinship_array, current_altaar, parcel_dim, num_days-i-1)
                 
         if total_yield_till_now + future_yield > max_expected_yield:
             max_expected_yield = total_yield_till_now + future_yield
             max_yield_level = current_altaar
-    
+        
         gotchi_kinship_array = [ele+2 for ele in gotchi_kinship_array]
 
     return max_expected_yield, max_yield_level
 
-    
-total_yield, altaar_level = gameplay_v3(gotchi_kinship_array=[500, 500],
-            num_days=300,
-            current_altaar=1,
-            parcel_dim = [32, 32],
-            max_allowed_altaar=3                                                
-           )
 
-#print(total_yield)
 
 
 
--- src/routes/home_page/api_home_page.py
+++ src/routes/home_page/api_home_page.py
@@ -14,30 +14,48 @@ from random import randrange
 
 
 class MyParcel:
-    level = 1
+    altaar_level = 0
     dim = (8, 8)
     yield_array = [0]*9
-    def __init__(self, level, dim, days, avg_gotchi):
-        self.level = level
+    gotchi_kinship_array = []
+    def __init__(self, altaar_level, dim, days, avg_gotchi):
+        self.altaar_level = altaar_level
         self.dim = dim
         yield_dict = compute_yield_dict(days, avg_gotchi)
         self.yield_array = yield_dict[dim]
+        self.gotchi_kinship_array = []
 
     def get_next_level_yield(self):
-        assert self.level < 8, "9 is the maximum available level"
-        return self.yield_array[self.level+1][0], self.yield_array[self.level+1][1]
+        assert self.altaar_level < 8, "9 is the maximum available level"
+        return self.yield_array[self.altaar_level+1][0], self.yield_array[self.altaar_level+1][1]
 
 
 class MyParcel_1:
-    level = 1
+    altaar_level = 1
+    reservior_level = 0
     dim = (8, 8)
     num_gotchi = 0
-    gotchi_kinship_array = []
-    def __init__(self, level, dim, num_gotchi):
-        self.level = level
+    gotchi_kinship_array = [] #this is the arrays which suggests what gotchis have been assigned to collect from this parcel
+    
+    def __init__(self, altaar_level, dim, num_gotchi):
+        self.altaar_level = altaar_level
         self.dim = dim
         self.num_gotchi = num_gotchi
         self.gotchi_kinship_array = []
+        
+    def reset_parcel(self):
+        #reset everything except the size
+        self.altaar_level = 0
+        self.num_gotchi = 0
+        self.gotchi_kinship_array = []
+        
+        
+    def print_parcel_info(self):
+        print("parcel size {} --> parcel level {} --> parcel gotchi {}".format(self.dim, self.altaar_level, self.num_gotchi))
+        channel_frequency = int(24/CHANNELING_RATE_IN_HOURS[self.altaar_level])
+        print("channel capacity of level {} num open slots {}".format(channel_frequency, channel_frequency - self.num_gotchi))
+
+
 
 
 
@@ -48,7 +66,7 @@ class MyParcel_1:
 class ClubHomePosts(Resource):
     def get(self):
         """
-        returns posts of Clubs
+        returns optimal levels for a givennumber of days
         :return:
 
             {
@@ -71,14 +89,14 @@ class ClubHomePosts(Resource):
         
         Parcels = []
 
-        for i in range(parcel_input[0]):
-            Parcels.append(MyParcel(0, (8, 8), days, avg_gotchi))
-            
-        for i in range(parcel_input[1]):
-            Parcels.append(MyParcel(0, (16, 16), days, avg_gotchi))
-            
         for i in range(parcel_input[2]):
             Parcels.append(MyParcel(0, (32, 64), days, avg_gotchi))
+
+        for i in range(parcel_input[1]):
+            Parcels.append(MyParcel(0, (16, 16), days, avg_gotchi))
+
+        for i in range(parcel_input[0]):
+            Parcels.append(MyParcel(0, (8, 8), days, avg_gotchi))
 
 
         num_available_gotchis = len(gotchi_kinship_array)
@@ -88,49 +106,73 @@ class ClubHomePosts(Resource):
             # check which is best among the upgradeable levels
             next_upgradeable_parcel = 0
             max_upgradeable_yield, _ = Parcels[0].get_next_level_yield()
-            
-            print("new loop")
-            print("num_available_gotchis is {}".format(num_available_gotchis))
-            
+
+            #print("new loop")
+            #print("num_available_gotchis is {}".format(num_available_gotchis))
+
             for i in range(len(Parcels)):
                 next_level_yield, level_constructed = Parcels[i].get_next_level_yield()
-                print("current level {} --- next level {} --> next yield {}".format(Parcels[i].level, level_constructed, next_level_yield))
-                
-                if level_constructed < Parcels[i].level:
+                #print("current level {} --- next level {} --> next yield {}".format(Parcels[i].altaar_level, level_constructed, next_level_yield))
+
+                if level_constructed < Parcels[i].altaar_level:
                     max_upgradeable_yield = -1
-                
+
                 elif next_level_yield > max_upgradeable_yield:
                     max_upgradeable_yield = next_level_yield
                     next_upgradeable_parcel = i
-            
-            
+
+
             #print(max_upgradeable_yield)
-            
+
             if max_upgradeable_yield < 0:
                 break
-            
-            
+
+
             #upgrade the index thus achieved, and reduce the num available gotchis
-            Parcels[next_upgradeable_parcel].level += 1
-            new_level = Parcels[next_upgradeable_parcel].level
-            
-            
-            channel_frequency = int(24/CHANNELING_RATE_IN_HOURS[altaar_level])
+            Parcels[next_upgradeable_parcel].altaar_level += 1
+            new_level = Parcels[next_upgradeable_parcel].altaar_level
+
+
+            #channel_frequency = int(24/CHANNELING_RATE_IN_HOURS[new_level])
             extra_available_channeling_slots = int(24/CHANNELING_RATE_IN_HOURS[new_level]) - int(24/CHANNELING_RATE_IN_HOURS[new_level - 1])
-            print("extra slots --> {}".format(extra_available_channeling_slots))
+            #print("extra slots --> {}".format(extra_available_channeling_slots))
             num_available_gotchis -= extra_available_channeling_slots
+            
+            for _ in range(extra_available_channeling_slots):
+                Parcels[next_upgradeable_parcel].gotchi_kinship_array.append(gotchi_kinship_array[0])
+                gotchi_kinship_array.pop(0)
 
 
-        result = {}
-        for i in range(len(Parcels)):
-            if str(Parcels[i].dim) in result:
-                result[str(Parcels[i].dim)].append(Parcels[i].level)
-            else:
-                result[str(Parcels[i].dim)] = [Parcels[i].level]
-            print("size {} level constructed {}".format(Parcels[i].dim, Parcels[i].level))
+        result = []
+        for parcel in Parcels:
+            pc = {}
+            pc['parcel_size'] =  " ".join([str(ele) for ele in list(parcel.dim)])
+            pc['altaar_level'] = parcel.altaar_level
+            pc['gotchis'] = ",".join([str(item) for item in parcel.gotchi_kinship_array])
+            result.append(pc)
+
+
+        breakeven_time, ty, py = get_breakeven_time(Parcels, days)
 
         
-        return make_response(jsonify(result), 200)
+        return make_response(jsonify({
+            "result": result,
+            "breakeven_time": breakeven_time,
+            "total_yield_in_fud": int(ty.get_total_yield_in_fud()),
+            "total_yield": {
+                "FUD": int(ty.fud),
+                "FOMO": int(ty.fomo),
+                "ALPHA": int(ty.alpha),
+                "KEK": int(ty.kek)
+            },
+            "profit_yield_in_fud": int(py.get_total_yield_in_fud()),
+            "profit_yield": {
+                "FUD": int(py.fud),
+                "FOMO": int(py.fomo),
+                "ALPHA": int(py.alpha),
+                "KEK": int(py.kek)
+            }
+        }), 200)
 
 
 
@@ -200,20 +242,72 @@ class ClubHomePosts1(Resource):
         for parcel in Parcels:
             #print("dim {} --> num gotchi {} --> {}".format(parcel.dim, parcel.num_gotchi, parcel.gotchi_kinship_array))
             
-            _, parcel.level = gameplay_v3(gotchi_kinship_array=parcel.gotchi_kinship_array,
+            _, parcel.altaar_level = gameplay_v3(gotchi_kinship_array=parcel.gotchi_kinship_array,
                         num_days=days,
                         current_altaar=1,
                         parcel_dim = parcel.dim,
                         max_allowed_altaar=9)
 
+        for parcel in Parcels:
+            parcel.print_parcel_info()
+        ########### post processing of the results ##################################
+        i = 0
+        j = len(Parcels)-1
+        final_channel_freq = max([parcel.num_gotchi for parcel in Parcels])
 
-        result = {}
-        for i in range(len(Parcels)):
-            if str(Parcels[i].dim) in result:
-                result[str(Parcels[i].dim)].append(Parcels[i].level)
-            else:
-                result[str(Parcels[i].dim)] = [Parcels[i].level]
-            print("size {} level constructed {}".format(Parcels[i].dim, Parcels[i].level))
+        while(i < j):
+
+            if final_channel_freq - Parcels[i].num_gotchi  <=0:
+                i += 1
+                continue
+            
+            while Parcels[j].num_gotchi > 0 and Parcels[i].num_gotchi < final_channel_freq:
+                gotchi = Parcels[j].gotchi_kinship_array.pop(0)
+                Parcels[j].num_gotchi -= 1
+                Parcels[i].gotchi_kinship_array.append(gotchi)
+                Parcels[i].num_gotchi += 1
+            
+            if Parcels[j].num_gotchi == 0:
+                #Parcels.pop(j)
+                Parcels[j].altaar_level = 0
+                j -= 1
+                
+            if Parcels[i].num_gotchi == final_channel_freq:
+                i += 1
+        ########## post processing ends here ############################################
+        for parcel in Parcels:
+            parcel.print_parcel_info()
 
 
-        return make_response(jsonify(result), 200)
+        result = []
+        for parcel in Parcels:
+            pc = {}
+            pc['parcel_size'] = " ".join([str(ele) for ele in list(parcel.dim)])
+            pc['altaar_level'] = parcel.altaar_level
+            pc['gotchis'] = ",".join([str(item) for item in parcel.gotchi_kinship_array])
+            result.append(pc)
+
+
+        breakeven_time, ty, py = get_breakeven_time(Parcels, days)
+
+        
+        return make_response(jsonify({
+            "result": result,
+            "breakeven_time": breakeven_time,
+            "total_yield_in_fud": int(ty.get_total_yield_in_fud()),
+            "total_yield": {
+                "FUD": int(ty.fud),
+                "FOMO": int(ty.fomo),
+                "ALPHA": int(ty.alpha),
+                "KEK": int(ty.kek)
+            },
+            "profit_yield_in_fud": int(py.get_total_yield_in_fud()),
+            "profit_yield": {
+                "FUD": int(py.fud),
+                "FOMO": int(py.fomo),
+                "ALPHA": int(py.alpha),
+                "KEK": int(py.kek)
+            }
+        }), 200)
+
+        #return make_response(jsonify(result), 200)
